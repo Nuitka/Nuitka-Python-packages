77a5ed064e31ed6891ec83b38728f89e6fa0c01c
Kay Hayen <kay.hayen@gmail.com>
2021-12-11 16:20:50 +0100

    Improved numpy patch, add 1.15 variant

---
+++ packages/np27-linux/numpy/build-1.15.py
@@ -0,0 +1,27 @@
+import __np__
+
+import sys, os
+
+
+def run(req,
+        temp_dir,
+        source_dir,
+        install_options,
+        global_options,
+        root,
+        home,
+        prefix,
+        warn_script_location,
+        use_user_site,
+        pycompilel
+        ):
+
+    __np__.setup_compiler_env()
+
+    os.chdir(source_dir)
+
+    __np__.my_print("Starting numpy patching...")
+    __np__.apply_patch(os.path.join(temp_dir, "numpy1.15-static-patch.patch"), ".")
+
+    __np__.my_print("Starting numpy compilation...")
+    __np__.run_with_output(sys.executable, os.path.join(source_dir, "setup.py"), "install")
