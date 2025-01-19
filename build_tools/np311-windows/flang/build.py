3d7425d23b60a9620bd5efdf0494c6a3de213cd6
Maxwell Dreytser <admin@mdtech.us>
2025-01-19 01:43:29 -0500

    Make sure the conda forge channel is enabled before getting flang.

--- build_tools/np311-windows/flang/build.py
+++ build_tools/np311-windows/flang/build.py
@@ -6,6 +6,8 @@ import os
 
 
 def run(temp_dir: str):
+    __np__.run_build_tool_exe("miniconda", "conda.exe", "config", "--add", "channels", "conda-forge")
+
     # It seems that miniconda is the only way to get prebuilt binaries for flang on windows :(
     # Revisit this when LLVM finally decides to publish windows binaries for flang.
     __np__.run_build_tool_exe("miniconda", "conda.exe", "create", "--prefix=" + temp_dir, "-y",
