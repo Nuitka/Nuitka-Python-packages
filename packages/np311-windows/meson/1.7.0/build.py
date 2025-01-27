fea9ef47af998b6b95b9887aaa5e80874748c8d8
Maxwell Dreytser <admin@mdtech.us>
2025-01-26 19:19:58 -0500

    Add meson 1.7.0 support.

---
+++ packages/np311-windows/meson/1.7.0/build.py
@@ -0,0 +1,21 @@
+import __np__
+import glob
+import shutil
+import sys
+import os
+import setuptools.build_meta
+
+
+def run(wheel_directory):
+    __np__.setup_compiler_env()
+
+    __np__.run_build_tool_exe("patch", "patch.exe", "-t", "-p1", "-i",
+                              os.path.join(os.path.dirname(__file__), "1.7.0", "meson-static-patch.patch"))
+
+    os.environ["PEP517_BACKEND_PATH"] = os.pathsep.join([x for x in sys.path if not x.endswith(os.path.sep + "site")])
+    __np__.run_with_output(sys.executable, "-m", "pip", "wheel", ".", "--verbose")
+
+    wheel_location = glob.glob("meson-*.whl")[0]
+    wheel_name = os.path.basename(wheel_location)
+    shutil.copy(wheel_location, os.path.join(wheel_directory, wheel_name))
+    return os.path.join(wheel_directory, wheel_name)
