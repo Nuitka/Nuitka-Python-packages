b2e1c16741d0c37e731b484a2b8dc83bf3343198
Maxwell Dreytser <admin@mdtech.us>
2025-02-02 19:08:00 -0500

    Resolve short paths when installing miniconda.

--- build_tools/np311-windows/miniconda/build.py
+++ build_tools/np311-windows/miniconda/build.py
@@ -13,6 +13,6 @@ def run(temp_dir: str):
     install_dir = os.path.join(temp_dir, "install")
     os.mkdir(install_dir)
     __np__.run_with_output(downloaded_file, "/InstallationType=JustMe", "/AddToPath=0", "/S", "/RegisterPython=0",
-                           "/NoRegistry=1", "/NoScripts=1", "/NoShortcuts=1", "/D=" + install_dir)
+                           "/NoRegistry=1", "/NoScripts=1", "/NoShortcuts=1", "/D=" + os.path.realpath(install_dir))
     os.rename(os.path.join(install_dir, "_conda.exe"), os.path.join(install_dir, "conda.exe"))
     __np__.install_build_tool("miniconda", os.path.join(install_dir, "*"))
