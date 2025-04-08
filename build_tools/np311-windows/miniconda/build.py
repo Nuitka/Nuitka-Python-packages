0bd033b13f957c844a5c38002269e6f8325ec698
Maxwell Dreytser <admin@mdtech.us>
2025-04-08 12:17:48 -0400

    miniconda: ignore errors due to long paths.

--- build_tools/np311-windows/miniconda/build.py
+++ build_tools/np311-windows/miniconda/build.py
@@ -9,4 +9,4 @@ def run(temp_dir: str):
     __np__.download_extract(
         "https://github.com/Nuitka/Nuitka-Python-packages/releases/download/dummy_miniconda_25.1.1/Miniconda3-py311_25.1.1-2-Windows-x86_64.zip",
         temp_dir)
-    __np__.install_build_tool("miniconda", os.path.join(temp_dir, "miniconda3", "*"))
+    __np__.install_build_tool("miniconda", os.path.join(temp_dir, "miniconda3", "*"), ignore_errors=True)
