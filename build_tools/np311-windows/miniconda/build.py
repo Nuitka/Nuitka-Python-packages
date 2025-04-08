a523fc77833825fbe9493098fbc01af0cec35da4
Maxwell Dreytser <admin@mdtech.us>
2025-04-07 22:03:59 -0400

    miniconda: Use our pre-setup zip to avoid issues with installer.

--- build_tools/np311-windows/miniconda/build.py
+++ build_tools/np311-windows/miniconda/build.py
@@ -6,13 +6,7 @@ import os
 
 
 def run(temp_dir: str):
-    downloaded_file = __np__.download_file(
-        "https://repo.anaconda.com/miniconda/Miniconda3-py311_24.11.1-0-Windows-x86_64.exe", temp_dir)
-
-    os.chdir(temp_dir)
-    install_dir = os.path.join(temp_dir, "install")
-    os.mkdir(install_dir)
-    __np__.run_with_output(downloaded_file, "/InstallationType=JustMe", "/AddToPath=0", "/S", "/RegisterPython=0",
-                           "/NoRegistry=1", "/NoScripts=1", "/NoShortcuts=1", "/D=" + os.path.realpath(install_dir))
-    os.rename(os.path.join(install_dir, "_conda.exe"), os.path.join(install_dir, "conda.exe"))
-    __np__.install_build_tool("miniconda", os.path.join(install_dir, "*"))
+    __np__.download_extract(
+        "https://github.com/Nuitka/Nuitka-Python-packages/releases/download/dummy_miniconda_25.1.1/Miniconda3-py311_25.1.1-2-Windows-x86_64.zip",
+        temp_dir)
+    __np__.install_build_tool("miniconda", os.path.join(temp_dir, "miniconda3", "*"))
