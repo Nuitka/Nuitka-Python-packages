7a3bf9e49261122c62cdf4e20f2904a9d5435e10
Maxwell Dreytser <admin@mdtech.us>
2025-04-10 00:54:52 -0400

    pandas: Remap some symbols to avoid collisions.

--- packages/np311-macos/pandas/build.py
+++ packages/np311-macos/pandas/build.py
@@ -4,17 +4,41 @@ import shutil
 import sys
 import os
 import setuptools.build_meta
+from tempfile import TemporaryDirectory
+from wheel.wheelfile import WheelFile
 
 
 def run(wheel_directory):
-    __np__.setup_compiler_env()
-
-    __np__.run_build_tool_exe("patch", "patch.exe", "-t", "-p1", "-i",
+    __np__.run_with_output("patch", "-t", "-p1", "-i",
                               os.path.join(os.path.dirname(__file__), "pandas-static-patch.patch"))
 
-    __np__.run_with_output(sys.executable, "-m", "pip", "wheel", ".", "--verbose")
+    env = os.environ.copy()
+    env["MACOSX_DEPLOYMENT_TARGET"] = "10.9"
+    env["PEP517_BACKEND_PATH"] = os.pathsep.join([x for x in sys.path if not x.endswith(os.path.sep + "site")])
+    env["PATH"] = (os.path.dirname(__np__.find_build_tool_exe("cmake", "cmake")) + os.pathsep +
+                   os.path.dirname(__np__.find_build_tool_exe("ninja", "ninja")) + os.pathsep + os.environ["PATH"])
+    __np__.run_with_output(sys.executable, "-m", "pip", "wheel", ".", "--verbose", "--no-build-isolation", env=env)
 
     wheel_location = glob.glob("pandas-*.whl")[0]
+
+    wheel_files = []
+    with TemporaryDirectory() as tmpdir:
+        with WheelFile(wheel_location) as wf:
+            for filename in wf.namelist():
+                wheel_files.append(filename)
+                wf.extract(filename, tmpdir)
+        __np__.rename_symbols_in_file(os.path.join(tmpdir, "pandas/_libs/interval.nuitkapython-311-darwin.a"), "pandas__libs_interval")
+        __np__.rename_symbols_in_file(os.path.join(tmpdir, "pandas/_libs/parsers.nuitkapython-311-darwin.a"), "pandas__libs_parsers")
+        __np__.rename_symbols_in_file(os.path.join(tmpdir, "pandas/_libs/algos.nuitkapython-311-darwin.a"), "pandas__libs_algos")
+        __np__.rename_symbols_in_file(os.path.join(tmpdir, "pandas/_libs/hashtable.nuitkapython-311-darwin.a"), "pandas__libs_hashtable")
+        __np__.rename_symbols_in_file(os.path.join(tmpdir, "pandas/_libs/join.nuitkapython-311-darwin.a"), "pandas__libs_join")
+        __np__.rename_symbols_in_file(os.path.join(tmpdir, "pandas/_libs/index.nuitkapython-311-darwin.a"), "pandas__libs_index")
+        __np__.rename_symbols_in_file(os.path.join(tmpdir, "pandas/_libs/pandas_datetime.nuitkapython-311-darwin.a"), "pandas__libs_pandas_datetime")
+        __np__.rename_symbols_in_file(os.path.join(tmpdir, "pandas/_libs/json.nuitkapython-311-darwin.a"), "pandas__libs_json")
+        with WheelFile(wheel_location, 'w') as wf:
+            for filename in wheel_files:
+                wf.write(os.path.join(tmpdir, filename), filename)
+
     wheel_name = os.path.basename(wheel_location)
     shutil.copy(wheel_location, os.path.join(wheel_directory, wheel_name))
     return os.path.join(wheel_directory, wheel_name)
