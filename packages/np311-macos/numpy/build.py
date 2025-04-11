b64da904c8e504726cfa973175bfcb8ca99a9384
Maxwell Dreytser <admin@mdtech.us>
2025-04-11 15:36:35 -0400

    numpy: Fix integrations with numpy.

--- packages/np311-macos/numpy/build.py
+++ packages/np311-macos/numpy/build.py
@@ -4,11 +4,15 @@ import shutil
 import sys
 import os
 import setuptools.build_meta
+from tempfile import TemporaryDirectory
+from wheel.wheelfile import WheelFile
 
 
 def run(wheel_directory):
     __np__.run_with_output("patch", "-p1", "-i",
                               os.path.join(os.path.dirname(__file__), "numpy-static-patch.patch"))
+
+    build_dir = os.getcwd()
 
     env = os.environ.copy()
     env["MACOSX_DEPLOYMENT_TARGET"] = "10.9"
@@ -25,6 +29,22 @@ def run(wheel_directory):
                "-Csetup-args=-Dblas=openblas", "-Csetup-args=-Dlapack=openblas", env=env)
 
     wheel_location = glob.glob("numpy-*.whl")[0]
+
+    wheel_files = []
+    with TemporaryDirectory() as tmpdir:
+        with WheelFile(wheel_location) as wf:
+            for filename in wf.namelist():
+                wheel_files.append(filename)
+                wf.extract(filename, tmpdir)
+
+        os.chdir(tmpdir)
+        __np__.run_with_output("patch", "-p1", "-f", "-i",
+                               os.path.join(os.path.dirname(__file__), "numpy-post.patch"))
+        os.chdir(build_dir)
+
+        with WheelFile(wheel_location, 'w') as wf:
+            for filename in wheel_files:
+                wf.write(os.path.join(tmpdir, filename), filename)
     wheel_name = os.path.basename(wheel_location)
     shutil.copy(wheel_location, os.path.join(wheel_directory, wheel_name))
     return os.path.join(wheel_directory, wheel_name)
