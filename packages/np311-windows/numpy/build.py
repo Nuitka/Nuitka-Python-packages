dc2d17a7313c3d72d79e2bf44702f6eca0ed83a4
Maxwell Dreytser <admin@mdtech.us>
2025-04-11 19:00:10 -0400

    numpy, scipy: Fix crashes in some modules

--- packages/np311-windows/numpy/build.py
+++ packages/np311-windows/numpy/build.py
@@ -4,10 +4,14 @@ import shutil
 import sys
 import os
 import setuptools.build_meta
+from tempfile import TemporaryDirectory
+from wheel.wheelfile import WheelFile
 
 
 def run(wheel_directory):
     __np__.setup_compiler_env()
+
+    build_dir = os.getcwd()
 
     __np__.run_build_tool_exe("patch", "patch.exe", "-p1", "-i",
                               os.path.join(os.path.dirname(__file__), "numpy-static-patch.patch"))
@@ -22,6 +26,23 @@ def run(wheel_directory):
                "-Csetup-args=-Dblas=openblas", "-Csetup-args=-Dlapack=openblas", env=env)
 
     wheel_location = glob.glob("numpy-*.whl")[0]
+
+    wheel_files = []
+    with TemporaryDirectory() as tmpdir:
+        with WheelFile(wheel_location) as wf:
+            for filename in wf.namelist():
+                wheel_files.append(filename)
+                wf.extract(filename, tmpdir)
+
+        os.chdir(tmpdir)
+        __np__.run_build_tool_exe("patch", "patch.exe", "-p1", "-i",
+                                  os.path.join(os.path.dirname(__file__), "numpy-post.patch"))
+        os.chdir(build_dir)
+
+        with WheelFile(wheel_location, 'w') as wf:
+            for filename in wheel_files:
+                wf.write(os.path.join(tmpdir, filename), filename)
+
     wheel_name = os.path.basename(wheel_location)
     shutil.copy(wheel_location, os.path.join(wheel_directory, wheel_name))
     return os.path.join(wheel_directory, wheel_name)
