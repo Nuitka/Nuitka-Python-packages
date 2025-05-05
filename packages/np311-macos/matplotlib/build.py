b672a7ecf63524adc9842e5320b4d44ae9bbda7a
Maxwell Dreytser <admin@mdtech.us>
2025-05-05 14:48:53 -0400

    matplotlib: Add matplotlib on mac.

---
+++ packages/np311-macos/matplotlib/build.py
@@ -0,0 +1,30 @@
+import __np__
+import glob
+import shutil
+import sys
+import os
+import setuptools.build_meta
+from tempfile import TemporaryDirectory
+
+import mesonpy
+
+from wheel.wheelfile import WheelFile
+
+
+def run(wheel_directory):
+    __np__.run_with_output("patch", "-t", "-p1", "-i",
+                              os.path.join(os.path.dirname(__file__), "matplotlib-static-patch.patch"))
+
+    os.environ["MACOSX_DEPLOYMENT_TARGET"] = "10.9"
+    os.environ["CMAKE_PREFIX_PATH"] = __np__.find_dep_root("freetype")
+    os.environ["PATH"] = (os.path.dirname(__np__.find_build_tool_exe("cmake", "cmake")) + os.pathsep +
+                   os.path.dirname(__np__.find_build_tool_exe("ninja", "ninja")) + os.pathsep + os.environ["PATH"])
+
+    __np__.run_with_output(sys.executable, "-m", "pip", "wheel", ".", "--verbose", "--no-build-isolation",
+                           "-Csetup-args=-Dsystem-freetype=True")
+
+    wheel_location = glob.glob("matplotlib-*.whl")[0]
+
+    wheel_name = os.path.basename(wheel_location)
+    shutil.copy(wheel_location, os.path.join(wheel_directory, wheel_name))
+    return os.path.join(wheel_directory, wheel_name)
