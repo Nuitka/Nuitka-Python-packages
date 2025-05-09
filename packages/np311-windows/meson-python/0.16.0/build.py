7b6d64c42430ec369ebaba46fe72edfd544987a3
Maxwell175 <admin@mdtech.us>
2025-05-09 08:27:59 -0400

    Update build.py

--- packages/np311-windows/meson-python/0.16.0/build.py
+++ packages/np311-windows/meson-python/0.16.0/build.py
@@ -17,5 +17,6 @@ def run(wheel_directory):
     __np__.run_with_output(sys.executable, "-m", "build", "-w", "--no-isolation")
 
     wheel_location = glob.glob(os.path.join("dist", "meson_python-*.whl"))[0]
+    wheel_name = os.path.basename(wheel_location)
     shutil.copy(wheel_location, os.path.join(wheel_directory, wheel_name))
     return os.path.join(wheel_directory, wheel_name)
