f16725d02577460ee73280f8cfa105adec9cd67c
Maxwell175 <admin@mdtech.us>
2025-09-07 12:31:29 -0400

    numpy: flang is now split up into another package in conda.

--- build_tools/np311-windows/flang/build.py
+++ build_tools/np311-windows/flang/build.py
@@ -11,7 +11,7 @@ def run(temp_dir: str):
     # It seems that miniconda is the only way to get prebuilt binaries for flang on windows :(
     # Revisit this when LLVM finally decides to publish windows binaries for flang.
     __np__.run_build_tool_exe("miniconda", "conda.exe", "create", "--prefix=" + temp_dir, "-y",
-                              "flang", "perl", "libflang")
+                              "flang", "perl", "libflang", "flang-rt_win-64")
 
     __np__.install_build_tool("flang", os.path.join(temp_dir, "Library", "*"))
 
